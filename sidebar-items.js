82663c1a9518e56002b620c26560878bbd7ea3d1
Nate Mara <natemara@gmail.com>
2015-06-05 23:25:17 -0400

    delete files

--- sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"fn":[["all_winds_aloft",""],["winds_aloft_for_station",""]],"struct":[["Wind","The wind data from one weather station at one altitude"],["WindsAloft","Weather data from one weather station at all altitudes"]]});
