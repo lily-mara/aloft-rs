ae5caf6cf5167fa71cbfe1b27104d425e7c8cbed
Nate Mara <natemara@gmail.com>
2015-06-06 03:28:45 +0000

    rebuild pages at 8843ead

---
+++ implementors/rustc_serialize/serialize/trait.Encodable.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['url'] = ["impl <a class='trait' href='rustc_serialize/serialize/trait.Encodable.html' title='rustc_serialize::serialize::Encodable'>Encodable</a> for <a class='struct' href='url/struct.Url.html' title='url::Url'>Url</a>",];implementors['hyper'] = ["impl <a class='trait' href='rustc_serialize/serialize/trait.Encodable.html' title='rustc_serialize::serialize::Encodable'>Encodable</a> for <a class='struct' href='hyper/struct.Url.html' title='hyper::Url'>Url</a>",];implementors['aloft'] = ["impl <a class='trait' href='rustc_serialize/serialize/trait.Encodable.html' title='rustc_serialize::serialize::Encodable'>Encodable</a> for <a class='struct' href='aloft/struct.Wind.html' title='aloft::Wind'>Wind</a>","impl <a class='trait' href='rustc_serialize/serialize/trait.Encodable.html' title='rustc_serialize::serialize::Encodable'>Encodable</a> for <a class='struct' href='aloft/struct.WindsAloft.html' title='aloft::WindsAloft'>WindsAloft</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
