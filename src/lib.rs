28f077c5998b8bf004c6e0fd60e76080010486d9
Nate Mara <natemara@gmail.com>
2015-06-06 21:20:28 -0400

    Completely change the API and move tests to own file

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,21 @@
-#![feature(plugin,collections)]
+#![feature(plugin,collections,core)]
 #![plugin(regex_macros)]
 extern crate rustc_serialize;
 extern crate regex;
 extern crate hyper;
+extern crate chrono;
+extern crate core;
 
 use std::io::Read;
 use hyper::Client;
 use regex::Regex;
+use chrono::offset::utc::UTC;
+use chrono::Timelike;
+
+#[cfg(test)]
+mod test;
 
 /// Interact with winds aloft data from NOAA
-
 
 const URL: &'static str = "http://aviationweather.gov/products/nws/all";
 static PATTERN: Regex = regex!(r"(?x)
@@ -27,9 +33,12 @@ static PATTERN: Regex = regex!(r"(?x)
 static WIND_PATTERN: Regex = regex!(r"(?x)
     \w+
 ");
+static TIME_PATTERN: Regex = regex!(r"(?x)
+    DATA\sBASED\sON\s(?P<date>\d{2})(?P<time>\d{4})Z
+");
 
 /// The wind data from one weather station at one altitude
-#[derive(Debug,RustcDecodable, RustcEncodable)]
+#[derive(Debug,RustcEncodable,RustcDecodable)]
 pub struct Wind {
     direction: u32,
     speed: u32,
@@ -37,10 +46,109 @@ pub struct Wind {
 }
 
 /// Weather data from one weather station at all altitudes
-#[derive(Debug,RustcDecodable, RustcEncodable)]
-pub struct WindsAloft {
+#[derive(Debug,RustcEncodable,RustcDecodable)]
+pub struct StationForecast {
     station: String,
     winds: Vec<Wind>,
+}
+
+/// All information associated with winds aloft forecast
+#[derive(Debug,RustcEncodable,RustcDecodable)]
+pub struct WindsAloftForecast {
+    /// The zulu time that this forecast was received as an unsigned 32-bit number. For example, if
+    /// the report was downloaded at 2:45 PM (UTC + 1), this field will be 1345u32.
+    time_retrieved: u32,
+
+    /// The zulu time that this report was generated.
+    forecast_time: u32,
+
+    /// The list of `StationForecast` objects that are held in this report.
+    forecasts: Vec<StationForecast>,
+}
+
+
+impl WindsAloftForecast {
+    pub fn new() -> WindsAloftForecast {
+        let mut forecast = WindsAloftForecast {
+            time_retrieved: 0u32,
+            forecast_time: 0u32,
+            forecasts: vec![],
+        };
+        forecast.refresh();
+
+        forecast
+    }
+
+    /// Re-downloads the forecast from NOAA. Returns None if there is a problem with the download,
+    /// or `Some(())` if the operation completes successfully.
+    pub fn refresh(&mut self) -> Option<()> {
+        let mut client = Client::new();
+
+        let mut res = match client.get(URL).send() {
+            Ok(x) => x,
+            Err(_) => return None,
+        };
+
+        let mut body = String::new();
+        res.read_to_string(&mut body).unwrap();
+
+        self.forecasts = winds_from_body(&body);
+        self.forecast_time = WindsAloftForecast::parse_time_from_body(&body);
+
+        let now = UTC::now();
+        let hour = now.hour();
+        let minute = now.minute();
+
+        self.time_retrieved = hour * 100 + minute;
+
+        Some(())
+    }
+
+    /// Returns true if the data stored in this forecast is out of date, false otherwise. The
+    /// winds aloft forecast is published at 00Z, 06Z, 12Z and 18Z
+    pub fn needs_refresh(&self) -> bool {
+        let now = UTC::now();
+        let hour = now.hour();
+        let minute = now.minute();
+
+        self.needs_refresh_time_given(hour * 100 + minute)
+    }
+
+    fn needs_refresh_time_given(&self, time: u32) -> bool {
+        let hour = time / 100u32;
+        let forecast_hour = self.forecast_time / 100u32;
+
+        if hour > forecast_hour + 6 {
+            true
+        } else if hour < forecast_hour {
+            true
+        } else {
+            false
+        }
+    }
+
+    /// Get the forecast for a specific station out of this complete forecast.
+    pub fn get_station_forecast(&self, station: &str) -> Option<&StationForecast> {
+        let upper_station = station.to_uppercase();
+        for wind in self.forecasts.iter() {
+            if wind.station == upper_station {
+                return Some(wind);
+            }
+        }
+        None
+    }
+
+    fn parse_time_from_body(body: &str) -> u32 {
+        let mut time = 0u32;
+        for group in TIME_PATTERN.captures_iter(body) {
+            time = match group.name("time") {
+                Some(x) => x.parse().unwrap(),
+                None => continue,
+            }
+        }
+
+        time
+    }
 }
 
 impl Wind {
@@ -53,9 +161,9 @@ impl Wind {
     }
 }
 
-impl WindsAloft {
-    pub fn new(station: &str, winds: Vec<Wind>) -> WindsAloft {
-        WindsAloft { station: station.to_string(), winds: winds }
+impl StationForecast {
+    pub fn new(station: &str, winds: Vec<Wind>) -> StationForecast {
+        StationForecast { station: station.to_string(), winds: winds }
     }
 
     /// Search through the wind data from this station, return the wind data from the given
@@ -70,39 +178,10 @@ impl WindsAloft {
     }
 }
 
-pub fn winds_aloft_for_station(station: &str) -> Option<WindsAloft> {
-    let upper_station = station.to_uppercase();
-    match all_winds_aloft() {
-        Some(winds) => {
-            for wind in winds {
-                if wind.station == upper_station {
-                    return Some(wind);
-                }
-            }
-        },
-        None => return None
-    }
-    None
-}
+fn winds_from_body(body: &str) -> Vec<StationForecast> {
+    let mut wind_aloft: Vec<StationForecast> = vec![];
 
-pub fn all_winds_aloft() -> Option<Vec<WindsAloft>> {
-    let mut client = Client::new();
-
-    let mut res = match client.get(URL).send() {
-        Ok(x) => x,
-        Err(_) => return None,
-    };
-
-    let mut body = String::new();
-    res.read_to_string(&mut body).unwrap();
-
-    Some(winds_from_body(body))
-}
-
-fn winds_from_body(body: String) -> Vec<WindsAloft> {
-    let mut wind_aloft: Vec<WindsAloft> = vec![];
-
-    for group in PATTERN.captures_iter(&body) {
+    for group in PATTERN.captures_iter(body) {
         let station = match group.name("station") {
             Some(x) => x,
             None => continue,
@@ -200,7 +279,7 @@ fn winds_from_body(body: String) -> Vec<WindsAloft> {
             None => { }
         }
 
-        wind_aloft.push(WindsAloft::new(station, winds));
+        wind_aloft.push(StationForecast::new(station, winds));
     }
 
     wind_aloft
@@ -224,43 +303,5 @@ fn parse_wind(wind: &str, altitude: u32) -> Option<Wind> {
         Some(Wind::new(direction, speed, altitude))
     } else {
         None
-    }
-}
-
-#[cfg(test)]
-mod test {
-    #[test]
-    fn parse_basic_wind() {
-        let wind = ::parse_wind("3127", 3000u32).unwrap();
-
-        assert_eq!(wind.speed, 27u32);
-        assert_eq!(wind.direction, 310u32);
-    }
-
-    #[test]
-    fn parse_bad_wind() {
-        let wind = ::parse_wind("invalid", 3000u32);
-        let parsed = match wind {
-            Some(_) => true,
-            None => false,
-        };
-
-        assert_eq!(parsed, false);
-    }
-
-    #[test]
-    fn parse_wind_with_negative_temp() {
-        let wind = ::parse_wind("3127-32", 3000u32).unwrap();
-
-        assert_eq!(wind.speed, 27u32);
-        assert_eq!(wind.direction, 310u32);
-    }
-
-    #[test]
-    fn parse_wind_with_positive_temp() {
-        let wind = ::parse_wind("3127+32", 3000u32).unwrap();
-
-        assert_eq!(wind.speed, 27u32);
-        assert_eq!(wind.direction, 310u32);
     }
 }
---
+++ src/test.rs
@@ -0,0 +1,124 @@
+#[test]
+fn parse_basic_wind() {
+    let wind = ::parse_wind("3127", 3000u32).unwrap();
+
+    assert_eq!(wind.speed, 27u32);
+    assert_eq!(wind.direction, 310u32);
+}
+
+#[test]
+fn parse_bad_wind() {
+    let wind = ::parse_wind("invalid", 3000u32);
+    let parsed = match wind {
+        Some(_) => true,
+        None => false,
+    };
+
+    assert_eq!(parsed, false);
+}
+
+#[test]
+fn parse_wind_with_negative_temp() {
+    let wind = ::parse_wind("3127-32", 3000u32).unwrap();
+
+    assert_eq!(wind.speed, 27u32);
+    assert_eq!(wind.direction, 310u32);
+}
+
+#[test]
+fn parse_wind_with_positive_temp() {
+    let wind = ::parse_wind("3127+32", 3000u32).unwrap();
+
+    assert_eq!(wind.speed, 27u32);
+    assert_eq!(wind.direction, 310u32);
+}
+
+#[test]
+fn need_update_0_12() {
+    let forecast = ::WindsAloftForecast {
+        forecast_time: 0000u32,
+        time_retrieved: 0000u32,
+        forecasts: Vec::new(),
+    };
+
+    assert!(forecast.needs_refresh_time_given(1200u32));
+    assert!(forecast.needs_refresh_time_given(1250u32));
+}
+
+#[test]
+fn need_update_12_12() {
+    let forecast = ::WindsAloftForecast {
+        forecast_time: 1200u32,
+        time_retrieved: 1200u32,
+        forecasts: Vec::new(),
+    };
+
+    assert!(!forecast.needs_refresh_time_given(1200u32));
+    assert!(!forecast.needs_refresh_time_given(1250u32));
+}
+
+#[test]
+fn need_update_12_10() {
+    let forecast = ::WindsAloftForecast {
+        forecast_time: 1200u32,
+        time_retrieved: 1200u32,
+        forecasts: Vec::new(),
+    };
+
+    assert!(forecast.needs_refresh_time_given(1000u32));
+    assert!(forecast.needs_refresh_time_given(1050u32));
+}
+
+#[test]
+fn need_update_12_0() {
+    let forecast = ::WindsAloftForecast {
+        forecast_time: 1200u32,
+        time_retrieved: 1200u32,
+        forecasts: Vec::new(),
+    };
+
+    assert!(forecast.needs_refresh_time_given(0000u32));
+    assert!(forecast.needs_refresh_time_given(0050u32));
+}
+
+#[test]
+fn need_update_18_0() {
+    let forecast = ::WindsAloftForecast {
+        forecast_time: 1800u32,
+        time_retrieved: 1800u32,
+        forecasts: Vec::new(),
+    };
+
+    assert!(forecast.needs_refresh_time_given(0000u32));
+    assert!(forecast.needs_refresh_time_given(0050u32));
+}
+
+#[test]
+fn test_parse_12_time_from_body() {
+    let body = "
+        000
+        FBUS31 KWNO 061406
+        FD1US1
+        DATA BASED ON 061200Z
+        VALID 061800Z   FOR USE 1400-2100Z. TEMPS NEG ABV 24000
+
+        FT  3000    6000    9000   12000   18000   24000  30000  34000  39000
+    ";
+
+    assert_eq!(::WindsAloftForecast::parse_time_from_body(body), 1200u32);
+}
+
+#[test]
+fn test_parse_1432_time_from_body() {
+    let body = "
+        000
+        FBUS31 KWNO 061406
+        FD1US1
+        DATA BASED ON 061432Z
+        VALID 061800Z   FOR USE 1400-2100Z. TEMPS NEG ABV 24000
+
+        FT  3000    6000    9000   12000   18000   24000  30000  34000  39000
+    ";
+
+    assert_eq!(::WindsAloftForecast::parse_time_from_body(body), 1432u32);
+}
