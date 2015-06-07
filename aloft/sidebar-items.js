dfb4b786f797e5b595d1c6dd0f93fedb8784866d
Nate Mara <natemara@gmail.com>
2015-06-07 01:29:23 +0000

    rebuild pages at e1c733a

--- aloft/sidebar-items.js
+++ aloft/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["all_winds_aloft",""],["winds_aloft_for_station",""]],"struct":[["Wind","The wind data from one weather station at one altitude"],["WindsAloft","Weather data from one weather station at all altitudes"]]});
+initSidebarItems({"struct":[["StationForecast","Weather data from one weather station at all altitudes"],["Wind","The wind data from one weather station at one altitude"],["WindsAloftForecast","All information associated with winds aloft forecast"]]});
